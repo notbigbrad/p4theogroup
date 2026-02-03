887ab0ef9399713097dde3fb7bf97925be8fb77e
notbigbrad <bradley.milne.1@gmail.com>
2026-02-03 00:15:18 +0000

    Working but with a logic error RK scheme with some modularity removed, LE reimplemented to use new scheme, TOV ready once interpolation  is ready

--- gen/Runge_Kutta.hpp
+++ gen/Runge_Kutta.hpp
@@ -8,16 +8,18 @@ using namespace std;
 namespace Runge_Kutta
 {
     //// GENERIC RUNGE-KUTTA SCHEME
-    template<int RK_num, int ODE_num>                                                 // Template for differing RK schemes and ODE counts
-    pair< vector<long double>, array<vector<long double>, ODE_num> >                  // Returns value of each solved ODE for all time steps
+    // template<int RK_num, int ODE_num>                                                               // Template for differing RK schemes and ODE counts
+    constexpr int ODE_num = 2;
+    constexpr int RK_num = 4;
+    pair< vector<long double>, array<vector<long double>, ODE_num> >                                // Returns value of each solved ODE for all time steps
     runge_kutta_scheme
     (
-        long double inital_conditions[ODE_num],                                       // Initial conditions
-        function<long double( long double r, long double y[ODE_num] )> ODES[ODE_num], // Array of ODEs
-        long double tableau[RK_num][RK_num],                                          // Butcher tableau
-        long double stepping_factors[RK_num],                                         // Time stepping factors
-        long double weighting_factors[RK_num],                                        // Weighted sum factors
-        long double h,                                                                // Stepping size
-        function<bool( long double r, long double y[ODE_num] )> solved                // Condition for ODE being solved
+        long double inital_conditions[ODE_num],                                                     // Initial conditions
+        array<function<long double( long double r, array<long double, ODE_num> y)>, ODE_num> ODES, // Array of ODEs
+        long double tableau[RK_num][RK_num],                                                        // Butcher tableau
+        long double stepping_factors[RK_num],                                                       // Time stepping factors
+        long double weighting_factors[RK_num],                                                      // Weighted sum factors
+        long double h,                                                                              // Stepping size
+        function<bool( long double r, array<long double, 2> y )> solved                              // Condition for ODE being solved
     );
 }
